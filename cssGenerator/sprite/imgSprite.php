58d1d4f158a39772c20140c7631641c5dbe15805
Michael Zabka <zabka.michael@seznam.cz>
2013-08-29 11:53:41 +0200

    automize all

--- cssGenerator/sprite/imgSprite.php
+++ cssGenerator/sprite/imgSprite.php
@@ -7,58 +7,109 @@
  * To change this template use File | Settings | File Templates.
  */
 
-header('content-type: text/css');
+header('content-type: text/html');
 
-$baseDir = __DIR__.'/imgs/';
-$dir = scandir($baseDir);
+define('BASE_DIR', __DIR__.'/');
+define('BUILD_DIR', __DIR__.'/../../build/');
 
-$spriteImageName = 'android-iconography-medium';
-$spriteCssName = 'android-iconography-medium';
-/* icon-large */
-$cols = 10;
-$height = $width = 32;
-$dx = 48.0;
-$dy = 48.0;
-$x = $y = 0;
-$prefix = '.icon-large';
-$xStart = 0;
+$types = array(
+/* android icon-large */
+	array(
+		'src' => 'android-dark/',
+		'spriteImageName' => 'android-iconography-large',
+		'spriteCssName' => 'android-iconography-large',
+		'cols' => 10,
+		'height' => 32,
+		'width' => 32,
+		'dx' => 48.0,
+		'dy' => 48.0,
+		'x' => 0,
+		'y' => 0,
+		'prefix' => '.icon-large',
+		'xStart' => 0,
+	),
 /**/
-/* icon-medium * /
-$cols = 10;
-$height = $width = 24;
-$dx = 36.0;
-$dy = 36.0;
-$x = $y = 0;
-$prefix = '.icon-medium';
-$xStart = 0;
+/* glyph icon-large */
+	array(
+		'src' => 'glyphicons/',
+		'spriteImageName' => 'glyphicon-large',
+		'spriteCssName' => 'glyphicon-large',
+		'cols' => 10,
+		'height' => 32,
+		'width' => 32,
+		'dx' => 48.0,
+		'dy' => 48.0,
+		'x' => 0,
+		'y' => 0,
+		'prefix' => '.icon-large',
+		'xStart' => 0,
+	),
 /**/
-/* icon-standard * /
-$cols = 10;
-$height = $width = 16;
-$dx = 24.0;
-$dy = 24.0;
-$x = $y = 0;
-$prefix = '.icon-standard';
-$xStart = 463+347;
+/* glyph icon-medium */
+	array(
+		'src' => 'glyphicons/',
+		'spriteImageName' => 'glyphicon-medium',
+		'spriteCssName' => 'glyphicon-medium',
+		'cols' => 10,
+		'height' => 24,
+		'width' => 24,
+		'dx' => 36.0,
+		'dy' => 36.0,
+		'x' => 0,
+		'y' => 0,
+		'prefix' => '.icon-medium',
+		'xStart' => 0,
+	),
 /**/
-/* icon-mini * /
-$cols = 10;
-$height = $width = 12;
-$dx = 18.0;
-$dy = 18.0;
-$x = $y = 0;
-$prefix = '.icon-mini';
-$xStart = 463+347+232;
+/*glyph  icon-standard */
+	array(
+		'src' => 'glyphicons/',
+		'spriteImageName' => 'glyphicon-standard',
+		'spriteCssName' => 'glyphicon-standard',
+		'cols' => 10,
+		'height' => 16,
+		'width' => 16,
+		'dx' => 24.0,
+		'dy' => 24.0,
+		'x' => 0,
+		'y' => 0,
+		'prefix' => '.icon-standard',
+		'xStart' => 0,
+	),
 /**/
-/* icon-new * /
-$cols = 10;
-$height = $width = 14;
-$dx = 21.0;
-$dy = 21.0;
-$x = $y = 0;
-$prefix = '.icon-new';
-$xStart = 0;//463+347+232+174;
+/* glyph icon-new */
+	array(
+		'src' => 'glyphicons/',
+		'spriteImageName' => 'glyphicon-new',
+		'spriteCssName' => 'glyphicon-new',
+		'cols' => 10,
+		'height' => 14,
+		'width' => 14,
+		'dx' => 21.0,
+		'dy' => 21.0,
+		'x' => 0,
+		'y' => 0,
+		'prefix' => '.icon-new',
+		'xStart' => 0,
+	),
 /**/
+/* glyph icon-mini */
+	array(
+		'src' => 'glyphicons/',
+		'spriteImageName' => 'glyphicon-mini',
+		'spriteCssName' => 'glyphicon-mini',
+		'cols' => 10,
+		'height' => 12,
+		'width' => 12,
+		'dx' => 18.0,
+		'dy' => 18.0,
+		'x' => 0,
+		'y' => 0,
+		'prefix' => '.icon-mini',
+		'xStart' => 0,
+	),
+/**/
+);
 
 function imagetransparent($width, $height) {
 	$trans = @imagecreatefrompng(__DIR__.'/transparent.png');
@@ -72,59 +123,99 @@ function imagetransparent($width, $height) {
 //header('content-type: image/png');
 //imagepng(imagetransparent(100, 100));die();
 
-$ob = ob_start();
+$table = '';
+$links = '<link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">';
 
-echo $prefix.' { background-image: url("../img/'.$spriteImageName.'.png"); width: '.$width.'px; height: '.$height.'px; }';
-echo "\n";
+function sprite($spriteImageName, $spriteCssName, $cols, $height, $width, $dx, $dy, $x, $y, $prefix, $xStart, $src) {
+	global $table, $links;
 
-$img = imagetransparent($cols*$dx+$xStart, (round(count($dir)/$cols)+1)*$dy);
+	$dir = scandir(BASE_DIR.$src);
 
-$i = 0;
-foreach ($dir as $file) {
-	$filename = $baseDir.$file;
-	$newX = round(($x-2+$xStart));
-	$newY = round(($y-3));
-	// image
-	$icon = @imagecreatefrompng($filename);
-	if (!$icon) continue;
+	$table.= '<table><thead><tr><th colspan="'.$cols.'">'.$prefix.'</th></tr></thead><tbody><tr>';
+	$ob = ob_start();
 
-	// Get new sizes
-	list($old_width, $old_height) = getimagesize($filename);
-	$newwidth = $width;
-	$newheight = $height;
-	// Load
-	if (true || $newwidth != $old_width) {
-		$thumbIcon = imagetransparent($newwidth, $newheight);
-		// Resize
-		$resized = imagecopyresized($thumbIcon, $icon, 0, 0, 0, 0, $newwidth, $newheight, $old_width, $old_height);
-		$icon = $thumbIcon;
-	}
-	// append
-	$merged = imagecopymerge($img, $icon, $newX, $newY, 0, 0, $newwidth, $newheight, 100);
+	$background = 'background-image: url("../img/'.$spriteImageName.'.png");';
 
-	preg_match('~^\d+_(.+).png$~', $file, $m);
-	if (!isset($m[1])) { continue; }
-	$name = $m[1];
-	$name = str_replace('_', '-', $name);
-
-	echo $prefix.'.icon-'.$name.' { background-position: '.(-$newX).'px '.(-$newY).'px; }';
+	echo $prefix.' { width: '.$width.'px; height: '.$height.'px; }';
 	echo "\n";
 
-	$x = $x + $dx;
-	$i++;
-	if ($i >= $cols) {
-		$i = 0;
-		$x = 0;
-		$y = $y + $dy;
+	$img = imagetransparent($cols*$dx+$xStart, (round(count($dir)/$cols)+1)*$dy);
+
+
+	$i = 0;
+	foreach ($dir as $file) {
+		$filename = BASE_DIR.$src.$file;
+		$newX = round(($x-2+$xStart));
+		$newY = round(($y-3));
+		// image
+		$icon = @imagecreatefrompng($filename);
+		if (!$icon) continue;
+
+		// Get new sizes
+		list($old_width, $old_height) = getimagesize($filename);
+		$newwidth = $width;
+		$newheight = $height;
+		// Load
+		if (true || $newwidth != $old_width) {
+			$thumbIcon = imagetransparent($newwidth, $newheight);
+			// Resize
+			$resized = imagecopyresized($thumbIcon, $icon, 0, 0, 0, 0, $newwidth, $newheight, $old_width, $old_height);
+			$icon = $thumbIcon;
+		}
+		// append
+		$merged = imagecopymerge($img, $icon, $newX, $newY, 0, 0, $newwidth, $newheight, 100);
+
+		preg_match('~^([a-zA-Z]+_)?\d+_(.+).png$~', $file, $m);
+		if (!isset($m[2])) { continue; }
+		$name = $m[2];
+		$name = str_replace('_', '-', $name);
+
+		echo $prefix.'.icon-'.$name.' { '.$background.' background-position: '.(-$newX).'px '.(-$newY).'px; }';
+		echo "\n";
+
+		$table.= '<td title="'.$prefix.'.icon-'.$name.'"><i class="'.str_replace('.', '', $prefix).' icon-'.$name.'"></i></td>';
+
+		$x = $x + $dx;
+		$i++;
+		if ($i >= $cols) {
+			$i = 0;
+			$x = 0;
+			$y = $y + $dy;
+			$table.= '</tr><tr>';
+		}
 	}
+	$css = ob_get_clean();
+	$table.= '</tr></tbody></table>';
+
+	file_put_contents(BUILD_DIR.'css/'.$spriteCssName.'.css', $css);
+	$links.= '<link rel="stylesheet" href="./css/'.$spriteCssName.'.css" />';
+
+	//header('content-type: image/png');
+	//imagepng($img);die();
+	$saved = imagepng($img, BUILD_DIR.'img/'.$spriteImageName.'.png');
+
+	if ($saved) echo 'sprite created '.$spriteImageName.' <br />';
+	else echo 'sprite failed '.$spriteImageName.' <br />';
+
 }
-$css = ob_get_clean();
 
-file_put_contents('./'.$spriteCssName.'.css', $css);
 
-//header('content-type: image/png');
-//imagepng($img);die();
-$saved = imagepng($img, './'.$spriteImageName.'.png');
+foreach ($types as $type) {
+	sprite($type['spriteImageName'], $type['spriteCssName'], $type['cols'], 
+		$type['height'], $type['width'], $type['dx'], $type['dy'], $type['x'], 
+		$type['y'], $type['prefix'], $type['xStart'], $type['src']
+	);
+}
 
-if ($saved) echo 'sprite vytvořen';
-else echo 'nastala chyba';
+$h = '<!DOCTYPE html>';
+$h.= '<html>';
+$h.= '<head>';
+$h.= $links;
+$h.= '</head>';
+$h.= '<body>';
+$h.= $table;
+$h.= '</body>';
+$h.= '</html>';
+
+
+file_put_contents(BUILD_DIR.'docs.html', $h);
